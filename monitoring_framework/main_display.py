4cafe6100cac0b99c0919548dc75046c77db0947
pennswood <normenyu@gmail.com>
2022-10-05 22:19:07 -0400

    added home page

--- monitoring_framework/main_display.py
+++ monitoring_framework/main_display.py
@@ -1,9 +1,12 @@
 import data_monitor
 import learned_model_monitor
+import home
 import streamlit as st
 
-version = st.radio("Visualization tool:", ["Data Visualization", "Learned Model Visualization"], horizontal = True)
+version = st.radio("Visualization tool:", ["Home", "Data Visualization", "Learned Model Visualization"], horizontal = True)
 
+if version == "Home":
+    home.main()
 if version == "Data Visualization":
     data_monitor.main()
 elif version == "Learned Model Visualization":
